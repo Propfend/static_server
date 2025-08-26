c2991b6156ba7895b3c7a857e214f974aac280d0
Propfend <mellomello2030@gmail.com>
2025-08-26 15:06:47 +0000

    add paddler

--- paddler.nix
+++ paddler.nix
@@ -1,16 +1,27 @@
 {
   stdenv,
   fetchFromGitHub,
+#   imlib2,
+#   xorg
 }:
 
 stdenv.mkDerivation rec {
   pname = "paddler";
-  version = "2.1.0";
+  version = "v2.1.0";
 
   src = fetchFromGitHub {
-    repo = "paddlerr";
+    repo = "${pname}";
     rev = "v2.1.0";
     owner = "intentee";
-    sha256 = "sha256-1kJjhtlsAkpNB7f6tZEs+dbKd8z7KoNHyDHEJ0tmhnc=";
+    sha256 = "sha256-aiLPVdKSppT/PWPW0Ue475WG61pBLh8OtLuk2/UU3nM=";
   };
+
+#   buildInputs = [ imlib2 xorg.libX11 ];
+
+#   installPhase = ''
+#     runHook preInstall
+#     mkdir -p $out/bin
+#     cp icat $out/bin
+#     runHook postInstall
+#   '';
 }
