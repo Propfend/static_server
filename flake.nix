491cd5161fc447c0dcebab2fc072ee96f891e3b3
Propfend <mellomello2030@gmail.com>
2025-09-01 11:51:30 +0000

    remove flake.nix

--- flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  inputs = {
-    nixpkgs.url = "github:Mic92/nixpkgs/master";
-    git-example-submodule.url = "git+https://github.com/Propfend/static_server.git";
-    flake = false;
-  };
-
-  outputs = { self, ... }@inputs: { ... };
-}
